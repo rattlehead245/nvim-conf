3fcf277322608ee0d832fec570b74fc94fcb09d6
rattlehead245 <84149041+rattlehead245@users.noreply.github.com>
2024-08-25 02:36:20 +0200

    Fixing indentation guides

--- init.lua
+++ init.lua
@@ -236,14 +236,17 @@ require('lazy').setup({
     },
   },
 
-  -- {
-  --   -- Add indentation guides even on blank lines
-  --   'lukas-reineke/indent-blankline.nvim',
-  --   -- Enable `lukas-reineke/indent-blankline.nvim`
-  --   -- See `:help ibl`
-  --   main = 'ibl',
-  --   opts = { enable = false },
-  -- },
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {
+      indent = { char = "|" },
+      whitespace = { highlight = { "Whitespace", "NonText" } },
+    },
+  },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
